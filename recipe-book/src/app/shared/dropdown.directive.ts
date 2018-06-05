8896b969decbdcc45b5b4eef78a6860d46abb64b
Pranav Kumar Verma <39586967+pranavtechnossus@users.noreply.github.com>
2018-06-05 15:36:16 +0530

    adding custom dropdown

---
+++ recipe-book/src/app/shared/dropdown.directive.ts
@@ -0,0 +1,14 @@
+import { Directive, HostListener, HostBinding } from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]'
+})
+export class DropdownDirective {
+@HostBinding('class.open')  isOpen = false;
+  constructor() { }
+
+  @HostListener('click') toggleOpen() {
+    this.isOpen = !this.isOpen;
+  }
+
+}
