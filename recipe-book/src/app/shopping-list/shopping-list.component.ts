ed3db7c3633c73edb57160190324168e7c6df0fe
Pranav Kumar Verma <39586967+pranavtechnossus@users.noreply.github.com>
2018-06-08 13:35:37 +0530

    Observable practices

--- recipe-book/src/app/shopping-list/shopping-list.component.ts
+++ recipe-book/src/app/shopping-list/shopping-list.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
+  private subscription: Subscription;
 
   constructor(private slService: ShoppingListService) { }
 
@@ -21,5 +23,8 @@ export class ShoppingListComponent implements OnInit {
           this.ingredients = ingredients;
         }
       );
+  }
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
